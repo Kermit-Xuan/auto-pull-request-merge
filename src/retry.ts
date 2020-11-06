4ad85ae55d903cfceea1c4d2d6b99e7543bc2e31
Kermit <kermitlx@outlook.com>
2020-11-06 17:18:13 +0800

    Revert "WIP Merge method" (#3)

--- src/retry.ts
+++ src/retry.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import * as core from '@actions/core'
 import dayjs from 'dayjs'
 
@@ -36,7 +35,7 @@ export default class Retry {
       try {
         return await f(count++)
       } catch (err) {
-        core.error(`Catch error for retry try ${err}`)
+        core.debug(`Catch error for retry try ${err}`)
       } finally {
         await this.wait()
       }
