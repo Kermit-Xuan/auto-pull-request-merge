94bc8644dfd2d554d1b4cb2b2241d9d26aa2f044
Kermit <kermitlx@outlook.com>
2020-11-06 17:07:57 +0800

    chore: add debug

--- src/retry.ts
+++ src/retry.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import * as core from '@actions/core'
 import dayjs from 'dayjs'
 
@@ -35,6 +36,7 @@ export default class Retry {
       try {
         return await f(count++)
       } catch (err) {
+        console.log(`Catch error for retry try ${err}`)
         core.debug(`Catch error for retry try ${err}`)
       } finally {
         await this.wait()
